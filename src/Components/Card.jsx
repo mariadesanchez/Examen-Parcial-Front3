b0465607636147e1752b6b51f04512971b8bdbf4
mariadesanchez <mariade_snchz@hotmail.com>
2023-06-03 22:22:49 -0300

    style color

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -6,7 +6,7 @@ const Card = ({name,mascota, color}) => {
   return (
     <div className="tarjeta" style={{border:'solid black 1.5px'}}>
         <h3>Hola {name}!</h3>
-        <h3>Sabemos que tu mascota facorita es {mascota}</h3>
+        <h3>Sabemos que tu mascota facorita es <span style={{color:color}}>{mascota}</span></h3>
         <h3>De color</h3>
         <span style={{backgroundColor: color}}>{color}</span>
     </div>
